3d807ba763c6931fdfc205bcf1b23fff8bc4b419
listyantidewi1 <dewi@smkn12malang.sch.id>
2024-01-29 15:07:36 +0700

    back to action bar

--- app/src/main/java/smkn12/tutorials/teachandroidnav/MainActivity.kt
+++ app/src/main/java/smkn12/tutorials/teachandroidnav/MainActivity.kt
@@ -3,18 +3,24 @@ package smkn12.tutorials.teachandroidnav
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import smkn12.tutorials.teachandroidnav.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding //membuat objek binding utk mengakses data dari layout
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //mengatur content view
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-
-
+        val navCtrl = this.findNavController(R.id.navhost_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navCtrl)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navCtrl = this.findNavController(R.id.navhost_fragment)
+        return navCtrl.navigateUp()
     }
 }

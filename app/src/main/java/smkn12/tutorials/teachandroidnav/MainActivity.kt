6bb22ea7882c9b882ad8a8c3f28d42952d47a05a
listyantidewi1 <dewi@smkn12malang.sch.id>
2024-02-01 10:12:52 +0700

    drawer layout on dashboard

--- app/src/main/java/smkn12/tutorials/teachandroidnav/MainActivity.kt
+++ app/src/main/java/smkn12/tutorials/teachandroidnav/MainActivity.kt
@@ -3,6 +3,7 @@ package smkn12.tutorials.teachandroidnav
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import smkn12.tutorials.teachandroidnav.databinding.ActivityMainBinding
@@ -10,17 +11,18 @@ import smkn12.tutorials.teachandroidnav.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
-
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //mengatur content view
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
+        drawerLayout = binding.drawerLayout
         val navCtrl = this.findNavController(R.id.navhost_fragment)
-        NavigationUI.setupActionBarWithNavController(this, navCtrl)
+        NavigationUI.setupActionBarWithNavController(this, navCtrl, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navCtrl)
     }
     override fun onSupportNavigateUp(): Boolean {
         val navCtrl = this.findNavController(R.id.navhost_fragment)
-        return navCtrl.navigateUp()
+        return NavigationUI.navigateUp(navCtrl, drawerLayout)
     }
 }
